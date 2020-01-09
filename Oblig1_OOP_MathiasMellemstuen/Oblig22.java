3f7df91505728560eb449fcb0595dba30e57c1ce
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-01-09 12:18:28 +0100

    Added task 2.2 and 2.3

--- Oblig1_OOP_MathiasMellemstuen/Oblig22.java
+++ Oblig1_OOP_MathiasMellemstuen/Oblig22.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Oblig22 {
 
-    public static double moonToEarthRatio = 0.17;
+    public static double moonToEarthPercentage = 0.17;
     public static double CalculateMoonWeightFromEarthWeight(double yourEarthWeight) {
 
-        return yourEarthWeight - (yourEarthWeight * moonToEarthRatio); 
+        return yourEarthWeight * moonToEarthPercentage; 
     }
 
     public static void main(String[] args) {
@@ -13,10 +13,10 @@ public class Oblig22 {
         System.out.println("Skriv inn din vekt på jorden (kg): ");
 
         Scanner scanner = new Scanner(System.in);
-        String userWeight = scanner.next();
+        double userWeight = scanner.nextDouble();
         scanner.close();
 
-        double weight = CalculateMoonWeightFromEarthWeight(Integer.parseInt(userWeight));
+        double weight = CalculateMoonWeightFromEarthWeight(userWeight);
         System.out.println("Din vekt på månen vil være: " + weight + " kg.");
 
     }
