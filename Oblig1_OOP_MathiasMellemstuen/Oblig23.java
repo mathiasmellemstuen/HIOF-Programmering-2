9068b84515bbf5f54402de2de2578023b6192e48
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-01-10 16:21:52 +0100

    Kommentert kode.

--- Oblig1_OOP_MathiasMellemstuen/Oblig23.java
+++ Oblig1_OOP_MathiasMellemstuen/Oblig23.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 
 
-class Oblig23 {
+public class Oblig23 {
     
     private static Scanner scanner; 
     private static ArrayList<Planet> planetList = new ArrayList<Planet>();
     private static boolean running = true; 
-    private static String planetsFilePath = "Planets.txt"; 
+    private static String planetsFilePath = "Planets.txt";
 
     private static void addNewPlanet() {
 
@@ -27,16 +27,18 @@ class Oblig23 {
         double gravity = scanner.nextDouble(); 
 
 
+        //Lager en ny planet fra verdiene skrevet inn i terminalen og legger den til i planetList. 
         Planet planet = new Planet(planetList.size(), name, radius, gravity); 
         planetList.add(planet);
         System.out.println("La til en ny planet: ");
-        planet.print(); 
+        planet.print(); // Printer den nye planetene til terminalen. 
     }
     private static void updatePlanet() {
         System.out.print("\nSkriv inn id på planeten du vil endre på: ");
 
         int id = scanner.nextInt(); 
 
+        //Sjekker om id'en som er tastet inn finnes. Hvis ikke så returnerer den. 
         if(id >= planetList.size() && id <= 0) {
             System.out.println("Finner ingen planet med id: " + id);
             return; 
@@ -51,6 +53,7 @@ class Oblig23 {
         System.out.print("Skriv inn gravitasjonen på planeten: ");
         double gravity = scanner.nextDouble(); 
 
+        //Oppdaterer verdiene som er skrevet inn på planeten. 
         planetList.get(id).setName(name);
         planetList.get(id).setRadius(radius);
         planetList.get(id).setGravity(gravity);
@@ -58,10 +61,12 @@ class Oblig23 {
         System.out.println("\nPlaneten er endret.\n");
     }
     private static void deletePlanet() {
+
         System.out.print("Skriv inn id på planeten: ");
 
         int id = scanner.nextInt();
 
+        //Sjekker om id'en finnes, fjerner planeten hvis den finnes.
         if(id < planetList.size() && id >= 0) {
 
             planetList.remove(id); 
@@ -75,6 +80,7 @@ class Oblig23 {
         
         System.out.println("Liste over alle planeter:\n" );
 
+        //Looper igjennom alle planetene i planetList og kaller print metoden. 
         for(int i = 0; i < planetList.size(); i++) {
 
             planetList.get(i).print();
@@ -89,10 +95,13 @@ class Oblig23 {
     
             Scanner fileScanner = new Scanner(file); 
             
+            //Looper igjennom alle linjene i filen. 
             while(fileScanner.hasNextLine()) {
 
+                // Lager en array ut av linjen med å splitte linjen med skille tegnet.
                 String[] splitted = fileScanner.nextLine().split(";"); 
 
+                //Lager en ny planet ut av verdiene fra splitted og legger den til i planetlist. 
                 Planet p = new Planet(planetList.size(),splitted[0],Double.parseDouble(splitted[1]),Double.parseDouble(splitted[2]));
                 planetList.add(p); 
             }
@@ -102,9 +111,6 @@ class Oblig23 {
         } catch(FileNotFoundException e) {
             System.out.println("Eksisterer " + planetsFilePath + " ?");
             System.out.println(e.toString());
-
-            //throw e; 
-
         } 
     }
     private static void writePlanetFile() {
@@ -114,14 +120,19 @@ class Oblig23 {
 
             FileWriter fileWriter = new FileWriter(file);
 
-            for(int i = 0; i < planetList.size(); i++) {
+            //Looper igjennom alle planeter i planetList og skriver det til Planets.txt, en planet per linje. 
+            for(int i = 0; i < planetList.size(); i++) { 
 
-                Planet p = planetList.get(i); 
+                Planet p = planetList.get(i);
                 
+                /*  Skriver planetens navn, radius og gravitasjon til filen. 
+                    Legger på et tegn på slutten slik at jeg senere kan skille mellom dem når jeg skal lese filen. 
+                */
                 fileWriter.write(p.getName() + ";");
                 fileWriter.write(p.getRadius() + ";");
                 fileWriter.write(p.getGravity() + ";");
 
+                //Legger til en ny linje med \n sålenge den ikke skriver den siste planeten / er på den siste linjen. 
                 if(planetList.size() > i - 1)
                     fileWriter.write("\n"); 
             }
@@ -153,15 +164,15 @@ class Oblig23 {
             break; 
             case 1: 
                 addNewPlanet(); 
-                writePlanetFile();
+                writePlanetFile(); // Skriver endringer til Planets.txt
             break; 
             case 2: 
                 updatePlanet(); 
-                writePlanetFile();
+                writePlanetFile(); // Skriver endringer til Planets.txt
             break; 
             case 3: 
                 deletePlanet();
-                writePlanetFile();
+                writePlanetFile(); // Skriver endringer til Planets.txt
             break; 
             case 4: 
                 listPlanets();
@@ -169,6 +180,14 @@ class Oblig23 {
             case 5: 
                 running = false; // Dette vil gjøre at while loopen i main ikke kjører lenger og programmet lukkes på riktig måte. 
             break; 
+            case 6:
+
+            // Easter egg :)
+            try {
+                String url_open ="https://www.youtube.com/watch?v=ngPBQvVuxlw";
+                java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
+            } catch(Exception e) {}
+            break;
             default: 
                 System.out.println(choice + " er feil.");
                 chooseAction();
@@ -177,13 +196,13 @@ class Oblig23 {
     }
     public static void main(String[] args) {
         
-        scanner = new Scanner(System.in); 
+        scanner = new Scanner(System.in); // Lager en ny scanner og setter System.in på den slik at den lytter etter input fra konsollen. 
 
-        readPlanetFile();
+        readPlanetFile(); // Leser Planets.txt og fyller ArrayList<Planet> planetList hvis det er innhold i Planets.txt
 
-        while(running) chooseAction();
+        while(running) chooseAction(); // chooseAction() vil loope sålenge running er true. 
         
-        scanner.close(); 
+        scanner.close(); // scanner.close() scanner når programmet lukkes. 
 
     }
 }
--- Oblig1_OOP_MathiasMellemstuen/Planet.java
+++ Oblig1_OOP_MathiasMellemstuen/Planet.java
@@ -1,10 +1,11 @@
 public class Planet {
 
-    private int id; 
+    private int id; //Id på planeten. Dette er det samme som index i listen over planeter. 
     private String name; 
     private double radius;
     private double gravity; 
     
+    // Getter / setters til name, radius, gravity, id. 
     public void SetId(int newId) {
         id = newId; 
     }
@@ -45,10 +46,21 @@ public class Planet {
         System.out.println("Gravity: " + gravity + " m/s²" + "\n");
     }
 
+    //Constructor. Alle variabler til planeten må settes i konstruktoren som parametere. 
     public Planet(int _id, String _name, double _radius, double _gravity) {
         id = _id; 
         name = _name; 
         radius = _radius; 
         gravity = _gravity; 
     }
+
+    /*
+    
+    Kan ha flere constructorer sålenge argumentene er annerledes. Passer derimot ikke i denne sammenhengen. 
+
+    public Planet() {
+
+    }
+
+    */
 }
